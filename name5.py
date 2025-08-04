3248390edfdef623fff85e352a6f8c0c339cd877
vido8 <vidyasr90@gmail.com>
2025-08-04 12:26:21 +0530

    Add files via upload

---
+++ name5.py
@@ -0,0 +1,18 @@
+user_name=input("enter your first name :")
+print("your name is:  ",user_name )
+
+
+users_name=input("enter your last name :")
+print("enter your name is: ",users_name)  
+
+print(user_name +users_name)
+
+full= input("your full name is:  ")
+
+print(full)
+
+
+greet=input("hello , johndoe welcome to ")
+
+print("greet")
+
---
+++ vidya5.py
@@ -0,0 +1,25 @@
+first = 5
+second =10
+number = input ("enter first number")
+print(number)
+number_s= input("enter the second number")
+print(number_s)
+addition=first+second
+print( addition)
+solve=input("addition is")
+print(solve)
+
+substraction = first-second
+print(substraction)
+solve_sub =input("substraction is")
+print(solve_sub)
+
+multiplication =first*second
+print(multiplication)
+solve_mul= input("multiplication is")
+print(solve_mul)
+
+division=first/second
+print(division)
+solve_div = input("division is")
+print(solve_div)
